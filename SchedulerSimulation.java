c8482efd5516c4ab1597826a4223ceee1dead6e2
sama-aleid <445052120@std.psau.edu.sa>
2026-03-24 03:59:21 +0300

    feature 1.java

--- SchedulerSimulation.java
+++ SchedulerSimulation.java
@@ -29,13 +29,22 @@ class Process implements Runnable {
     private int burstTime; // Total time the process requires to complete (in milliseconds)
     private int timeQuantum; // Time slice (time quantum) allowed per CPU access (in milliseconds)
     private int remainingTime; // Time left for the process to finish its execution
-
+    private int priority;
+    private long creationTime;
+    private long waitingTime;
     // Constructor to initialize the process with name, burst time, and time quantum
     public Process(String name, int burstTime, int timeQuantum) {
         this.name = name;
         this.burstTime = burstTime;
         this.timeQuantum = timeQuantum;
         this.remainingTime = burstTime; // Initially, remaining time is equal to the burst time
+
+        //(1-5)
+        Random rand = new Random();
+        this.priority=rand.nextInt(5) +1;
+
+        this.creationTime=System.currentTimeMillis();
+        this.waitingTime=0;
     }
 
     // This method will be called when the thread for this process is started
